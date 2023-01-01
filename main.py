407bf384347fd19af77b1f181d1e02c3024c928b
DindyalM <dindyal.mursingh@gmail.com>
2023-01-01 16:41:21 -0500

    debug deta

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
 from fastapi import FastAPI
+
+
+
+
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    url='https://www.fashionnova.com/collections/dresses'
+    count = 0
+    a =[]
+    #grab our main links
+    res=requests.get(url)
+    soup=BeautifulSoup(res.text,'html.parser')
+    dress_divs = soup.find_all('div', class_='collection-list__product-tile')
+    for div in dress_divs:
+        count = count  + 1
+        a.append(div.find('a').attrs['href'])
+        if(count==5):break
+
+    return {a[0]}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+
+
